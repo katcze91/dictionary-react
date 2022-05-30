a45b2ecd806648d4167f0d28dc1411356d811395
katcze91 <91422768+katcze91@users.noreply.github.com>
2022-05-30 18:16:49 +0200

    updated results

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from "react";
-import Dictionary from "./Dictionary"
+import Dictionary from "./Dictionary";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <h1>hello</h1>
-          <a href="#" className="btn btn-primary">
-            {" "}
-            Hello
-          </a>
+          <h1>Dictionary</h1>
         </header>
         <main>
-          < Dictionary/>
+          <Dictionary />
         </main>
         <footer>Coded by Katarzyna Czepiela</footer>
       </div>
     </div>
   );
 }
-
-export default App;
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,28 +1,31 @@
 import React, { useState } from "react";
+import Results from "./Results";
 import axios from "axios";
 
 export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+  let [keyword, setKeyword] = useState("book");
+  let [results, setResults] = useState(null);
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
   function search(event) {
     event.preventDefault();
-   
+     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+     axios.get(apiUrl).then(handleResponse);
   }
-  function handleResponse(response){
-      console.log(response.data[0])
+
+  function handleResponse(response) {
+    setResults(response.data[0]);
   }
-  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-  axios.get(apiUrl).then(handleResponse)
-  
+ 
   return (
     <div className="dictionary">
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
         <input type="submit" />
       </form>
+      <Results results={results} />
     </div>
   );
 }
