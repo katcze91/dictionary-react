57aa03680af1346652726497ff507e6cefa40407
katcze91 <91422768+katcze91@users.noreply.github.com>
2022-05-31 08:53:47 +0200

    styling changed

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Meaning.css"
 
 export default function Meaning(props) {
   return (
@@ -7,11 +8,7 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
-              {definition.definition}
-              <br />
-              <em>{definition.example}</em>
-            </p>
+            <p>{definition.definition}</p>
           </div>
         );
       })}
